6bbbfb96502b3df1edaaa508a1a8479bf5f5629c
jorshkun <137836656+jorshkun@users.noreply.github.com>
2024-03-07 19:53:39 -0700

    Add files via upload

---
+++ Ejercicio-JavaScript/script/ejercicio.js
@@ -0,0 +1,11 @@
+function cambiarColor(){
+    variable_album= document.getElementById("album");
+    variable_album.style.backgroundColor="red";
+    console.log("A") ;
+}
+function cambiarTamano(){
+    variable_imagen= document.getElementsByClassName("imagen");
+    variable_imagen[0].style.height="1000px";
+    variable_imagen[0].style.width="1000px";
+    console.log("B") ;
+}
